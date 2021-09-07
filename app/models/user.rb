b384937b9537e883b6a22c307053004ea083b7d1
Julio Noguera <noguerajulio24@gmail.com>
2021-09-06 22:28:28 -0500

    add trackable

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,19 @@
 #
 #  id                     :bigint           not null, primary key
 #  bio                    :string
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  last_name              :string
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string
 #  name                   :string
 #  notification_ofert     :boolean
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
 #  slug                   :string
 #  terms_condition        :boolean
 #  created_at             :datetime         not null
@@ -31,12 +36,17 @@ class User < ApplicationRecord
   has_many :user_contacts
 
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable,
+    :trackable
   #:omniauthable, omniauth_providers: %i[google_oauth2 facebook]
 
   validates :name, :email, :terms_condition, presence: true
 
   friendly_id :name, use: :slugged
+
+  def should_generate_new_friendly_id?
+    name_changed?
+  end
 
   def self.from_omniauth(access_token)
     user = User.where(email: access_token.info.email).first
