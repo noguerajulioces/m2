ded91fa7bc29a409589e5f754459072d067827ee
Julio Noguera <noguerajulio24@gmail.com>
2021-09-30 20:58:21 -0500

    update

--- app/webpack/packs/application.js
+++ app/webpack/packs/application.js
@@ -6,8 +6,8 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "channels"
 import "controllers"
+import "channels"
 import 'bootstrap'
 import "../src/theme";
 
