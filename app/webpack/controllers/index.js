fc7b58d523b964191a75aac03637ab8a780d8df5
Julio Noguera <noguerajulio24@gmail.com>
2021-10-01 18:16:53 -0500

    add stimulus

--- app/webpack/controllers/index.js
+++ app/webpack/controllers/index.js
@@ -1,6 +1,12 @@
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
+import TextareaAutogrow from "stimulus-textarea-autogrow"
+import CharacterCounter from "stimulus-character-counter"
+
 const application = Application.start(document.documentElement)
 const context = require.context('.', true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
+
+application.register("textarea-autogrow", TextareaAutogrow)
+application.register("character-counter", CharacterCounter)
